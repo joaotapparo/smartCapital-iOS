bc9b8a79178cdec4ae28ec2afeb369d45d3897fc
“Joao <joaotapparo@protonmail.com>
2025-02-25 20:37:15 -0300

    Initial Commit

---
+++ SmartCapital/SmartCapitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCapitalApp.swift
+//  SmartCapital
+//
+//  Created by Joao Tapparo on 25/02/25.
+//
+
+import SwiftUI
+
+@main
+struct SmartCapitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SmartCapitalTests/SmartCapitalTests.swift
@@ -0,0 +1,17 @@
+//
+//  SmartCapitalTests.swift
+//  SmartCapitalTests
+//
+//  Created by Joao Tapparo on 25/02/25.
+//
+
+import Testing
+@testable import SmartCapital
+
+struct SmartCapitalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
